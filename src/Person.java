da39796acee8aa0b1f917e80a3c323001c7c06a4
Daniyar pp <championyshka@mail.ru>
2025-12-25 22:24:38 +0300

    commit 4

--- src/Person.java
+++ src/Person.java
@@ -3,24 +3,7 @@ public class Person {
     protected String surname;
     protected Gender gender;
 
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
+    
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
